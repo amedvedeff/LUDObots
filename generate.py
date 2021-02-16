024d5fbeb02614d60fa2949ea532492ff2952803
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-15 22:02:24 -0500

    Ten boxes on top of eachother

--- generate.py
+++ generate.py
@@ -5,10 +5,9 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-x2 = 1
-y2 = 0
-z2 = 1.5  
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length, width, height])
+for r in range(10):
+	currBox = "Box" + str(x)
+	pyrosim.Send_Cube(name= currBox, pos=[x,y,z] , size=[length, width, height])
+	z += 1
 pyrosim.End()
