f0c7123a36d6f1c7e69eb206989f136488f6eb85
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-15 21:53:51 -0500

    Two boxes, one in front of the other

--- generate.py
+++ generate.py
@@ -1,10 +1,14 @@
 import pyrosim.pyrosim as pyrosim
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 x = 0
 y = 0
-z = 1.5 
-pyrosim.Start_SDF("box.sdf")
+z = 0.5
+x2 = 1
+y2 = 0
+z2 = 1.5  
+pyrosim.Start_SDF("boxes.sdf")
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length, width, height])
 pyrosim.End()
