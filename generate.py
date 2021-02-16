fe187e18f2572e6a5ce29ab28a87e8782f14b614
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-15 23:06:53 -0500

    Generates world sdf

--- generate.py
+++ generate.py
@@ -5,6 +5,6 @@ height = 1.0
 x = 0
 y = 0
 z = 0.5
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 pyrosim.Send_Cube(name= "Box", pos=[x,y,z] , size=[length, width, height])
 pyrosim.End()
