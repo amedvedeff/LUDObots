99e6e602fa6d2234ed068f0ef050d06e71cb7ee4
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-15 20:58:05 -0500

    Adding variables

--- simulate.py
+++ simulate.py
@@ -1,12 +1,12 @@
-import pybullet_data
 import time
 import pybullet as p
+import pybullet_data
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
-for x in range (1000):
+for x in range (60):
 	p.stepSimulation()
-	time.sleep(1.0/60.0)
+	time.sleep(1)
 p.disconnect()
