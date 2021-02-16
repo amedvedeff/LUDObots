47124d39a4e17944255a856908e11a7c368d28ec
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-15 23:05:27 -0500

    Generates one lonely box

--- generate.py
+++ generate.py
@@ -6,5 +6,5 @@ x = 0
 y = 0
 z = 0.5
 pyrosim.Start_SDF("boxes.sdf")
-prosim.Send_Cube(name= "Box", pos=[x,y,z] , size=[length, width, height])
+pyrosim.Send_Cube(name= "Box", pos=[x,y,z] , size=[length, width, height])
 pyrosim.End()
