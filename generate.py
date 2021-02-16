f87d5b69ba17a6facf7e39f089fe8770e53c021d
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-16 10:40:38 -0500

    Complete two joint, three link robot

--- generate.py
+++ generate.py
@@ -1,10 +1,23 @@
 import pyrosim.pyrosim as pyrosim
-length = 1.0
-width = 1.0
-height = 1.0
-x = 0
-y = 0
-z = 0.5
-pyrosim.Start_SDF("world.sdf")
-pyrosim.Send_Cube(name= "Box", pos=[x,y,z] , size=[length, width, height])
-pyrosim.End()
+def Create_World():
+	length = 1.0
+	width = 1.0
+	height = 1.0
+	x = -2
+	y = 2
+	z = 0.5
+	pyrosim.Start_SDF("world.sdf")
+	pyrosim.Send_Cube(name= "Box", pos=[x,y,z] , size=[length, width, height])
+	pyrosim.End()
+	
+def Create_Robot():
+	pyrosim.Start_URDF("body.urdf")
+	pyrosim.Send_Cube(name= "BackLeg", pos=[0.5, 0, 0.5] , size=[1, 1, 1])
+	pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg" , child = "Torso" , type = "revolute", position = "1.0 0 1.0")
+	pyrosim.Send_Cube(name= "Torso", pos=[0.5, 0, 0.5] , size=[1, 1, 1])
+	pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = "1.0 0 0")
+	pyrosim.Send_Cube(name= "FrontLeg", pos=[0.5, 0, -0.5] , size=[1, 1, 1])
+	pyrosim.End()
+
+Create_World()
+Create_Robot()
