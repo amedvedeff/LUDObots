cc84ee7e9b9c0657808adf34a2c566e1613af514
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-15 21:36:45 -0500

    Making a working box with variables

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
 length = 1
-width = 1
-height = 1
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[(length),(height),(width)])
+width = 2
+height = 3
+pyrosim.Start_SDF("box.sdf")
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
 pyrosim.End()
