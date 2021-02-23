1fc20422c797a4df239bb2221bb710a061244099
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-02-22 21:58:38 -0500

    Plotting leg touches

--- analyze.py
+++ analyze.py
@@ -1,1 +1,10 @@
 import numpy
+import matplotlib.pyplot
+
+backLegSensorValues = numpy.load(r'C:\Users\Administrator\Documents\LUDObots\data\backLegValues.npy')
+frontLegSensorValues = numpy.load(r'C:\Users\Administrator\Documents\LUDObots\data\frontLegValues.npy')
+line1 = matplotlib.pyplot.plot(backLegSensorValues, linewidth=3, label = 'Back leg')
+line2 = matplotlib.pyplot.plot(frontLegSensorValues, label = 'Front leg')
+matplotlib.pyplot.legend()
+
+matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -14,13 +14,15 @@ bodyId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
 backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 
 for x in range(0, 1000):
 	p.stepSimulation()
 	backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 	time.sleep(1/60)
 p.disconnect()
 
-print(backLegSensorValues)
 
 numpy.save(r'C:\Users\Administrator\Documents\LUDObots\data\backLegValues.npy', backLegSensorValues)
+numpy.save(r'C:\Users\Administrator\Documents\LUDObots\data\frontLegValues.npy', frontLegSensorValues)
