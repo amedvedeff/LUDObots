3ab2a0e3a832a454c3d32343eb8bc569bbb9564b
Alex Medvedeff <medvedeffalexander@gmail.com>
2021-03-04 13:35:38 -0500

    Finally able to move the legs and analyze the chart for their movments

--- analyze.py
+++ analyze.py
@@ -1,9 +1,9 @@
 import numpy
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load(r'C:\Users\Administrator\Documents\LUDObots\data\backLegValues.npy')
-frontLegSensorValues = numpy.load(r'C:\Users\Administrator\Documents\LUDObots\data\frontLegValues.npy')
-line1 = matplotlib.pyplot.plot(backLegSensorValues, linewidth=3, label = 'Back leg')
+backLegSensorValues = numpy.load(r'C:\Users\Administrator\Documents\LUDObots\data\backLegAngleValues.npy')
+frontLegSensorValues = numpy.load(r'C:\Users\Administrator\Documents\LUDObots\data\frontLegAngleValues.npy')
+line1 = matplotlib.pyplot.plot(backLegSensorValues,  label = 'Back leg')
 line2 = matplotlib.pyplot.plot(frontLegSensorValues, label = 'Front leg')
 matplotlib.pyplot.legend()
 
--- simulate.py
+++ simulate.py
@@ -3,23 +3,57 @@ import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import numpy
+import math
+import random
 import pathlib
 
 physicsClient = p.connect(p.GUI)
-
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+numSteps = 1000
+
+frontLeg_amplitude = numpy.pi / 10
+frontLeg_frequency = 10
+frontLeg_phaseOffset = numpy.pi
+
+frontLeg_targetAgles = frontLeg_amplitude * numpy.sin(frontLeg_frequency * numpy.linspace(-numpy.pi, numpy.pi, numSteps) + frontLeg_phaseOffset)
+
+backLeg_amplitude = numpy.pi / 4
+backLeg_frequency = 10
+backLeg_phaseOffset = 0
+
+backLeg_targetAgles = backLeg_amplitude * numpy.sin(backLeg_frequency * numpy.linspace(-numpy.pi, numpy.pi, numSteps) + backLeg_phaseOffset)
+#backLeg_targetAgles = numpy.sin(numpy.linspace(-numpy.pi, numpy.pi, 1000)) * numpy.pi / 4
+
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-bodyId = p.loadURDF("body.urdf")
+robot = p.loadURDF("body.urdf")
+
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = numpy.zeros(numSteps)
+frontLegSensorValues = numpy.zeros(numSteps)
 
-for x in range(0, 1000):
+numpy.save(r'C:\Users\Administrator\Documents\LUDObots\data\backLegAngleValues.npy', backLeg_targetAgles)
+numpy.save(r'C:\Users\Administrator\Documents\LUDObots\data\frontLegAngleValues.npy', frontLeg_targetAgles)
+
+
+for x in range (0, 1000):
 	p.stepSimulation()
 	backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+	pyrosim.Set_Motor_For_Joint(
+    	bodyIndex = robot,
+    	jointName = "BackLeg_Torso",
+    	controlMode = p.POSITION_CONTROL,
+    	targetPosition = backLeg_targetAgles[x],
+    	maxForce = 50)
+	pyrosim.Set_Motor_For_Joint(
+		bodyIndex = robot,
+		jointName = "Torso_FrontLeg",
+		controlMode = p.POSITION_CONTROL,
+		targetPosition = frontLeg_targetAgles[x],
+		maxForce = 50)
 	time.sleep(1/60)
 p.disconnect()
 
